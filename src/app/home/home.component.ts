c5fbad1a45a1d8e1b43d4e776ff776e2a66f9347
DiMNDev <Dimndev@gmail.com>
2025-09-26 15:44:31 -0600

    updated home component logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, InputSignal } from '@angular/core';
+import { Component, input, InputSignal, signal } from '@angular/core';
 import { BodyComponent } from '../body/body.component';
 
 @Component({
@@ -9,4 +9,12 @@ import { BodyComponent } from '../body/body.component';
 })
 export class HomeComponent {
   messageFromApp: InputSignal<string> = input<string>('');
+  messageToBody = signal<string>('');
+  ngOnChanges(): void {
+    this.messageToBody.set('NGOnChanges Called');
+  }
+
+  callAlert(message: string) {
+    alert('Home Component: ' + message);
+  }
 }
