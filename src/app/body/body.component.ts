d4dc8f11f7b367d8608abae95ff839a911e6a96a
DiMNDev <Dimndev@gmail.com>
2025-09-26 15:44:46 -0600

    updated body component logic

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  input,
+  InputSignal,
+  output,
+} from '@angular/core';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-body',
   imports: [],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.scss'
+  styleUrl: './body.component.scss',
 })
 export class BodyComponent {
+  messageFromHome: InputSignal<string> = input<string>('');
+  callAlertFromHome = output<string>();
 
+  buttonClicked(): void {
+    this.callAlertFromHome.emit('Button clicked!');
+  }
 }
